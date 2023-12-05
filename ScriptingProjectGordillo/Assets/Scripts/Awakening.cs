6c5ae70effec646e88ebe33a4103a8ad2aa18c9a
Maximiliano Gordillo <724910@student.dusd.net>
2023-12-05 14:47:11 -0800

    Awaking
    did the awaking project.

---
+++ ScriptingProjectGordillo/Assets/Scripts/Awakening.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awakening : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake is called.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
