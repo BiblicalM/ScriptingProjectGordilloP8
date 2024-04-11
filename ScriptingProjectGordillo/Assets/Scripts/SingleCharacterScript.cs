24940484da29f89de575e7c7a6a4c6ef5c60aac0
Maximiliano Gordillo <724910@student.dusd.net>
2024-04-11 15:26:37 -0700

    ALMOST DONE

--- ScriptingProjectGordillo/Assets/Scripts/SingleCharacterScript.cs
+++ ScriptingProjectGordillo/Assets/Scripts/SingleCharacterScript.cs
@@ -37,5 +37,18 @@ public class SingleCharacterScript : MonoBehaviour
     {
         float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+
+    void Shoot()
+    {
+        if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            myStuff.bullets--;
+        }
     }
 }
